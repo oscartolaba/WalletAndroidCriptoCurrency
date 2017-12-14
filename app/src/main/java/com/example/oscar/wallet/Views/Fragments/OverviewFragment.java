c7a24e75a1ed69e1c8b6df0edf63504a8bc3ffef
Oscar Tolaba <oscartolaba12@gmail.com>
2017-12-14 19:59:19 -0300

    fragments

--- app/src/main/java/com/example/oscar/wallet/Views/Fragments/OverviewFragment.java
+++ app/src/main/java/com/example/oscar/wallet/Views/Fragments/OverviewFragment.java
@@ -2,6 +2,7 @@ package com.example.oscar.wallet.Views.Fragments;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,6 +21,7 @@ import com.example.oscar.wallet.Adapters.Overview;
 import com.example.oscar.wallet.Adapters.OverviewAdapter;
 import com.example.oscar.wallet.Interfaces.OnClickOverviewListener;
 import com.example.oscar.wallet.R;
+import com.example.oscar.wallet.Views.Activities.IncomeActivity;
 
 import java.util.ArrayList;
 import java.util.function.ToLongBiFunction;
@@ -53,6 +56,15 @@ public class OverviewFragment extends Fragment {
         loadItemsRecyclerView();
         ItemOverviewAdapter adapter= new ItemOverviewAdapter(getActivity().getApplicationContext(),items);
         listViewOverview.setAdapter(adapter);
+        listViewOverview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Overview overview= (Overview) adapterView.getItemAtPosition(i);
+                navigateToListOperations(overview);
+            }
+
+
+        });
         /*  OverviewAdapter adapter= new OverviewAdapter(items);
         recyclerViewOverview.setAdapter(adapter);
 
@@ -64,7 +76,12 @@ public class OverviewFragment extends Fragment {
         });*/
         return view;
     }
+    private void navigateToListOperations(Overview overview) {
+       switch (overview.getId()){
+           case 1:startActivity(new Intent(getActivity().getApplicationContext(), IncomeActivity.class));break;
 
+       }
+    }
     private void loadItemsRecyclerView() {
         items.add(new Overview(1,"Cuenta","saldo acumulado",232.3));
         items.add(new Overview(2,"Ingresos","saldo pendiente",100.3));
